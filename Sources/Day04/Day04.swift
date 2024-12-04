55f1c996573bf0909e36f6de72301e8d88b622c2
Gereon Steffens <github@steffens.org>
2024-12-04 08:43:52 +0100

    day 4

--- Sources/Day04/Day04.swift
+++ Sources/Day04/Day04.swift
@@ -7,15 +7,75 @@
 import AoCTools
 
 final class Day04: AOCDay {
-    
+    let title = "Ceres Search"
+    let grid: [Point: Character]
+
     init(input: String) {
+        let points = input
+            .lines
+            .enumerated().flatMap { y, line in
+                line.enumerated().map { x, ch in
+                    let p = Point(x, y)
+                    return (p, ch)
+                }
+            }
+        grid = Dictionary(points, uniquingKeysWith: { _, new in new } )
     }
 
     func part1() -> Int {
-        0
+        grid.keys
+            .map {
+                findXMAS(at: $0)
+            }
+            .reduce(0, +) / 2
+    }
+
+    private func findXMAS(at p: Point) -> Int {
+        guard ["X", "S"].contains(grid[p]) else { return 0 }
+
+        let sets = Direction.allCases.map { dir in
+            (0...3).map { steps in
+                p.moved(to: dir, steps: steps)
+            }
+        }
+
+        let search: [Character] = ["X", "M", "A", "S"]
+        let searchRev = Array(search.reversed())
+
+        return sets
+            .filter { set in
+                let cmp = set.map {
+                    grid[$0, default: " "]
+                }
+                return cmp == search || cmp == searchRev
+            }
+            .count
     }
 
     func part2() -> Int {
-        0
+        grid.keys
+            .filter {
+                findX_MAS(at: $0)
+            }
+            .count
+    }
+
+    private func findX_MAS(at p: Point) -> Bool {
+        guard grid[p] == "A" else { return false }
+        
+        let sets = [
+            [p.moved(to: .nw), p, p.moved(to: .se)],
+            [p.moved(to: .sw), p, p.moved(to: .ne)]
+        ]
+
+        let search: [Character] = ["M", "A", "S"]
+        let searchRev = Array(search.reversed())
+
+        return sets.allSatisfy { set in
+            let cmp = set.map {
+                grid[$0, default: " "]
+            }
+            return cmp == search || cmp == searchRev
+        }
     }
 }
