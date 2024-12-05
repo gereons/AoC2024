8a2516353b64ec0919a77a9dfb7f971c758d0a6c
Gereon Steffens <github@steffens.org>
2024-12-05 18:35:21 +0100

    minor cleanup

--- Sources/Day05/Day05.swift
+++ Sources/Day05/Day05.swift
@@ -20,21 +20,32 @@ final class Day05: AOCDay {
     init(input: String) {
         let groups = input.lines.grouped(by: \.isEmpty)
 
-        rules = Set(groups[0]
+        rules = groups[0]
             .map { $0.allInts() }
             .map { Rule(page1: $0[0], page2: $0[1]) }
-        )
+            .makeSet()
 
         updates = groups[1]
             .map { $0.allInts() }
     }
 
     func part1() -> Int {
-        updates.reduce(into: 0) { sum, update in
-            if updateOk(update) {
-                sum += update[update.count / 2]
+        updates
+            .filter { updateOk($0) }
+            .map { $0.median() }
+            .reduce(0, +)
+    }
+
+    func part2() -> Int {
+        updates
+            .filter { !updateOk($0) }
+            .map {
+                $0.sorted { p1, p2 in
+                    rules.contains(Rule(page1: p1, page2: p2))
+                }
             }
-        }
+            .map { $0.median() }
+            .reduce(0, +)
     }
 
     private func updateOk(_ update: [Int]) -> Bool {
@@ -42,17 +53,5 @@ final class Day05: AOCDay {
             .combinations(ofCount: 2)
             .map { Rule(page1: $0[0], page2: $0[1]) }
             .allSatisfy { rules.contains($0) }
-    }
-
-
-    func part2() -> Int {
-        updates
-            .filter { !updateOk($0) }
-            .reduce(into: 0) { sum, invalid in
-                let sorted = invalid.sorted { p1, p2 in
-                    rules.contains(Rule(page1: p1, page2: p2))
-                }
-                sum += sorted[sorted.count / 2]
-            }
     }
 }
