759905b5706d13412e2c4cca0a1177501ed01051
Gereon Steffens <github@steffens.org>
2024-12-10 08:30:50 +0100

    day 10

--- Sources/Day10/Day10.swift
+++ Sources/Day10/Day10.swift
@@ -7,16 +7,60 @@
 import AoCTools
 
 final class Day10: AOCDay {
-    let title = ""
+    let title = "Hoof It"
+
+    let grid: [Point: Int]
 
     init(input: String) {
+        let lines = input.lines
+        let points = lines
+            .enumerated().flatMap { y, line in
+                line.enumerated().map { x, ch in
+                    let p = Point(x, y)
+                    return (p, Int(String(ch)) ?? -1)
+                }
+            }
+        grid = Dictionary(points, uniquingKeysWith: { _, new in new } )
     }
 
     func part1() -> Int {
-        0
+        let starts = grid.filter { $0.value == 0 }.map { $0.key }
+        let ends = grid.filter { $0.value == 9 }.map { $0.key }
+
+        let pathfinder = AStarPathfinder(map: Trail(grid: grid))
+
+        return starts.flatMap { start in
+            ends.map { end in
+                pathfinder.shortestPath(from: start, to: end) != [] ? 1 : 0
+            }
+        }.reduce(0, +)
     }
 
     func part2() -> Int {
-        0
+        let starts = grid.filter { $0.value == 0 }.map { $0.key }
+        let trail = Trail(grid: grid)
+
+        return starts
+            .reduce(0) { $0 + trail.findRatings(from: $1) }
+    }
+
+    struct Trail: Pathfinding {
+        let grid: [Point: Int]
+
+        func neighbors(for point: Point) -> [Point] {
+            let height = grid[point]!
+            return point
+                .neighbors()
+                .filter { grid[$0] == height + 1 }
+        }
+
+        func findRatings(from start: Point, rating: Int = 0) -> Int {
+            if grid[start] == 9 {
+                return rating + 1
+            }
+
+            return neighbors(for: start)
+                .reduce(0) { $0 + findRatings(from: $1, rating: rating) }
+        }
     }
 }
--- Tests/Day10Tests.swift
+++ Tests/Day10Tests.swift
@@ -6,6 +6,33 @@ import Testing
 @testable import AdventOfCode
 
 fileprivate let testInput = """
+89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
+"""
+
+fileprivate let testInput2 = """
+.....0.
+..4321.
+..5..2.
+..6543.
+..7..4.
+..8765.
+..9....
+"""
+
+fileprivate let testInput3 = """
+012345
+123456
+234567
+345678
+4.6789
+56789.
 """
 
 @Suite("Day 10 Tests")
@@ -13,24 +40,30 @@ struct Day10Tests {
     @MainActor @Test("Day 10 Part 1")
     func testDay10_part1() {
         let day = Day10(input: testInput)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 36)
     }
 
     @MainActor @Test("Day 10 Part 1 Solution")
     func testDay10_part1_solution() {
         let day = Day10(input: Day10.input)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 501)
     }
 
     @MainActor @Test("Day 10 Part 2")
     func testDay10_part2() {
-        let day = Day10(input: testInput)
-        #expect(day.part2() == 0)
+        var day = Day10(input: testInput)
+        #expect(day.part2() == 81)
+
+        day = Day10(input: testInput2)
+        #expect(day.part2() == 3)
+
+        day = Day10(input: testInput3)
+        #expect(day.part2() == 227)
     }
 
     @MainActor @Test("Day 10 Part 2 Solution")
     func testDay10_part2_solution() {
         let day = Day10(input: Day10.input)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == 1017)
     }
 }
