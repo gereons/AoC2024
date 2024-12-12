234fa01f4e0a4f1bc538eb3286a6c97c5a8d6416
Gereon Steffens <gereon.steffens@onvista.de>
2024-12-12 13:46:08 +0100

    day 12 part 1

--- Sources/Day12/Day12.swift
+++ Sources/Day12/Day12.swift
@@ -5,15 +5,67 @@
 //
 
 import AoCTools
+import Collections
 
 final class Day12: AOCDay {
-    let title = ""
+    let title = "Garden Groups"
+
+    let grid: [Point: Character]
 
     init(input: String) {
+        let points = input.lines
+            .enumerated().flatMap { y, line in
+                line.enumerated().map { x, ch in
+                    let p = Point(x, y)
+                    return (p, ch)
+                }
+            }
+        grid = Dictionary(points, uniquingKeysWith: { _, new in new } )
     }
 
     func part1() -> Int {
-        0
+        var grid = self.grid
+
+        var regions = [[Point: Character]]()
+
+        // var queue = Deque([(Point(53, 13), Character("G"))])
+
+        while let (point, ch) = grid.first {
+            // print("start search", point, ch)
+            var region = [Point: Character]()
+
+            var queue = Set([point])
+            while let p = queue.popFirst() {
+                region[p] = ch
+
+                let n = p.neighbors().filter {
+                    grid[$0] == ch && !region.keys.contains($0)
+                }
+                queue.formUnion(n)
+            }
+            // print("found region", region)
+            regions.append(region)
+
+            region.keys.forEach {
+                grid[$0] = nil
+            }
+        }
+
+        return regions.map {
+            $0.count * perimeter(of: $0)
+        }
+        .reduce(0, +)
+    }
+
+    private func perimeter(of region: [Point: Character]) -> Int {
+        var perimeter = 0
+        let regionKeys = Set(region.keys)
+        for p in regionKeys {
+            let p = 4 - p.neighbors().filter { regionKeys.contains($0) }.count
+            perimeter += p
+        }
+        // print("region", region, perimeter)
+        return perimeter
     }
 
     func part2() -> Int {
--- Tests/Day12Tests.swift
+++ Tests/Day12Tests.swift
@@ -6,26 +6,83 @@ import Testing
 @testable import AdventOfCode
 
 fileprivate let testInput = """
+AAAA
+BBCD
+BBCC
+EEEC
+"""
+
+fileprivate let testInput2 = """
+OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO
+"""
+
+fileprivate let testInput3 = """
+RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE
+"""
+
+fileprivate let testInput4 = """
+EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE
+"""
+
+fileprivate let testInput5 = """
+AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA
 """
 
 @Suite("Day 12 Tests")
 struct Day12Tests {
     @MainActor @Test("Day 12 Part 1")
     func testDay12_part1() {
-        let day = Day12(input: testInput)
-        #expect(day.part1() == 0)
+        var day = Day12(input: testInput)
+        #expect(day.part1() == 140)
+
+        day = Day12(input: testInput2)
+        #expect(day.part1() == 772)
+
+        day = Day12(input: testInput3)
+        #expect(day.part1() == 1930)
     }
 
     @MainActor @Test("Day 12 Part 1 Solution")
     func testDay12_part1_solution() {
         let day = Day12(input: Day12.input)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 1375574)
     }
 
     @MainActor @Test("Day 12 Part 2")
     func testDay12_part2() {
-        let day = Day12(input: testInput)
-        #expect(day.part2() == 0)
+//        var day = Day12(input: testInput)
+//        #expect(day.part1() == 80)
+//
+//        day = Day12(input: testInput2)
+//        #expect(day.part1() == 436)
+//
+//        day = Day12(input: testInput4)
+//        #expect(day.part1() == 236)
+//
+//        day = Day12(input: testInput5)
+//        #expect(day.part1() == 368)
     }
 
     @MainActor @Test("Day 12 Part 2 Solution")
