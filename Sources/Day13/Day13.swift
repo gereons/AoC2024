70c03ec41b446504f771adb690dc6fd0b61bdcad
Gereon Steffens <github@steffens.org>
2024-12-13 09:45:22 +0100

    day 13

--- Sources/Day13/Day13.swift
+++ Sources/Day13/Day13.swift
@@ -6,17 +6,60 @@
 
 import AoCTools
 
+private struct Machine {
+    let buttonA: Point
+    let buttonB: Point
+    let prize: Point
+}
+
 final class Day13: AOCDay {
-    let title = ""
-    
+    let title = "Claw Contraption"
+
+    private let machines: [Machine]
+
     init(input: String) {
+        let groups = input.lines.grouped(by: \.isEmpty)
+
+        machines = groups.map {
+            let btnA = $0[0].allInts()
+            let btnB = $0[1].allInts()
+            let prize = $0[2].allInts()
+            return Machine(
+                buttonA: Point(btnA[0], btnA[1]),
+                buttonB: Point(btnB[0], btnB[1]),
+                prize: Point(prize[0], prize[1])
+            )
+        }
     }
 
     func part1() -> Int {
-        0
+        machines
+            .compactMap { pushButtons($0) }
+            .reduce(0) { $0 + $1.0 * 3 + $1.1 }
     }
 
     func part2() -> Int {
-        0
+        machines
+            .compactMap { pushButtons($0, offset: 10_000_000_000_000) }
+            .reduce(0) { $0 + $1.0 * 3 + $1.1 }
+    }
+
+    private func pushButtons(_ m: Machine, offset: Int = 0) -> (Int, Int)? {
+        let prize = m.prize + Point(offset, offset)
+        let a =
+            (m.buttonB.x * -prize.y + m.buttonB.y * prize.x) /
+            (m.buttonB.y * m.buttonA.x - m.buttonB.x * m.buttonA.y)
+
+        let b =
+            -(m.buttonA.x * prize.y - m.buttonA.y * prize.x) /
+            (m.buttonB.x * m.buttonA.y - m.buttonB.y * m.buttonA.x)
+
+        let x = m.buttonA.x * a + m.buttonB.x * b
+        let y = m.buttonA.y * a + m.buttonB.y * b
+        if prize == Point(x, y) {
+            return (a, b)
+        } else {
+            return nil
+        }
     }
 }
--- Tests/Day13Tests.swift
+++ Tests/Day13Tests.swift
@@ -6,6 +6,21 @@ import Testing
 @testable import AdventOfCode
 
 fileprivate let testInput = """
+Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279
 """
 
 @Suite("Day 13 Tests")
@@ -13,24 +28,24 @@ struct Day13Tests {
     @MainActor @Test("Day 13 Part 1")
     func testDay13_part1() {
         let day = Day13(input: testInput)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 480)
     }
 
     @MainActor @Test("Day 13 Part 1 Solution")
     func testDay13_part1_solution() {
         let day = Day13(input: Day13.input)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 26810)
     }
 
     @MainActor @Test("Day 13 Part 2")
     func testDay13_part2() {
         let day = Day13(input: testInput)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == 875318608908)
     }
 
     @MainActor @Test("Day 13 Part 2 Solution")
     func testDay13_part2_solution() {
         let day = Day13(input: Day13.input)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == 108713182988244)
     }
 }
