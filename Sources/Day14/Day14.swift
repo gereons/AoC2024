36824940587c513bc3a653852ac9d05baaf8d5da
Gereon Steffens <github@steffens.org>
2024-12-14 10:19:08 +0100

    day 14

--- Sources/Day14/Day14.swift
+++ Sources/Day14/Day14.swift
@@ -6,17 +6,90 @@
 
 import AoCTools
 
+private class Robot {
+    var position: Point
+    let start: Point
+    let velocity: Point
+
+    init(position: Point, velocity: Point) {
+        self.start = position
+        self.position = position
+        self.velocity = velocity
+    }
+
+    func reset() {
+        position = start
+    }
+}
+
 final class Day14: AOCDay {
-    let title = ""
-    
-    init(input: String) {
+    let title = "Restroom Redoubt"
+
+    private let robots: [Robot]
+    let width: Int
+    let height: Int
+
+    convenience init(input: String) {
+        self.init(input: input, width: 101, height: 103)
+    }
+
+    init(input: String, width: Int, height: Int) {
+        robots = input.lines
+            .map { $0.allInts() }
+            .map { Robot(position: Point($0[0], $0[1]), velocity: Point($0[2], $0[3])) }
+        self.height = height
+        self.width = width
     }
 
     func part1() -> Int {
-        0
+        robots.forEach { $0.reset() }
+        for _ in 0..<100 {
+            for var robot in robots {
+                move(&robot)
+            }
+        }
+
+        let q1 = robots.filter { $0.position.x < width / 2 && $0.position.y < height / 2 }.count
+        let q2 = robots.filter { $0.position.x > width / 2 && $0.position.y < height / 2 }.count
+        let q3 = robots.filter { $0.position.x < width / 2 && $0.position.y > height / 2 }.count
+        let q4 = robots.filter { $0.position.x > width / 2 && $0.position.y > height / 2 }.count
+
+        return q1 * q2 * q3 * q4
+    }
+
+    private func move(_ robot: inout Robot) {
+        let newPosition = robot.position + robot.velocity
+        var newX = newPosition.x
+        var newY = newPosition.y
+        if newX < 0 {
+            newX = width + newX
+        } else if newX >= width {
+            newX -= width
+        }
+        if newY < 0 {
+            newY = height + newY
+        } else if newY >= height {
+            newY -= height
+        }
+        robot.position = Point(newX, newY)
     }
 
     func part2() -> Int {
-        0
+        // solved visually by looking at the output of
+        /*
+        robots.forEach { $0.reset() }
+
+        for i in 1 ... width * height {
+            for var robot in robots {
+                move(&robot)
+            }
+
+            print(i)
+            let map = Dictionary(robots.map { ($0.position, true) }, uniquingKeysWith: { _, new in new })
+            let grid = Grid(points: map)
+            grid.draw()
+        }
+        */
+        return 7492
     }
 }
--- Tests/Day14Tests.swift
+++ Tests/Day14Tests.swift
@@ -6,31 +6,37 @@ import Testing
 @testable import AdventOfCode
 
 fileprivate let testInput = """
+p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3
 """
 
 @Suite("Day 14 Tests")
 struct Day14Tests {
     @MainActor @Test("Day 14 Part 1")
     func testDay14_part1() {
-        let day = Day14(input: testInput)
-        #expect(day.part1() == 0)
+        let day = Day14(input: testInput, width: 11, height: 7)
+        #expect(day.part1() == 12)
     }
 
     @MainActor @Test("Day 14 Part 1 Solution")
     func testDay14_part1_solution() {
         let day = Day14(input: Day14.input)
-        #expect(day.part1() == 0)
-    }
-
-    @MainActor @Test("Day 14 Part 2")
-    func testDay14_part2() {
-        let day = Day14(input: testInput)
-        #expect(day.part2() == 0)
+        #expect(day.part1() == 208437768)
     }
 
     @MainActor @Test("Day 14 Part 2 Solution")
     func testDay14_part2_solution() {
         let day = Day14(input: Day14.input)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == 7492)
     }
 }
