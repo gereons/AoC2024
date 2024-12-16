8eeeb987ca0830ba8bdfc075f578e8afb78e5955
Gereon Steffens <github@steffens.org>
2024-12-16 21:50:53 +0100

    day 16

--- Tests/Day16Tests.swift
+++ Tests/Day16Tests.swift
@@ -6,31 +6,72 @@ import Testing
 @testable import AdventOfCode
 
 fileprivate let testInput = """
+###############
+#.......#....E#
+#.#.###.#.###.#
+#.....#.#...#.#
+#.###.#####.#.#
+#.#.#.......#.#
+#.#.#####.###.#
+#...........#.#
+###.#.#####.#.#
+#...#.....#.#.#
+#.#.#.###.#.#.#
+#.....#...#.#.#
+#.###.#.#.#.#.#
+#S..#.....#...#
+###############
+"""
+
+fileprivate let testInput2 = """
+#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################
 """
 
 @Suite("Day 16 Tests")
 struct Day16Tests {
     @MainActor @Test("Day 16 Part 1")
     func testDay16_part1() {
-        let day = Day16(input: testInput)
-        #expect(day.part1() == 0)
+        var day = Day16(input: testInput)
+        #expect(day.part1() == 7036)
+
+        day = Day16(input: testInput2)
+        #expect(day.part1() == 11048)
     }
 
     @MainActor @Test("Day 16 Part 1 Solution")
     func testDay16_part1_solution() {
         let day = Day16(input: Day16.input)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 111480)
     }
 
     @MainActor @Test("Day 16 Part 2")
     func testDay16_part2() {
-        let day = Day16(input: testInput)
-        #expect(day.part2() == 0)
+        var day = Day16(input: testInput)
+        #expect(day.part2() == 45)
+
+        day = Day16(input: testInput2)
+        #expect(day.part2() == 64)
     }
 
     @MainActor @Test("Day 16 Part 2 Solution")
     func testDay16_part2_solution() {
         let day = Day16(input: Day16.input)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == 529)
     }
 }
