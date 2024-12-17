3e4f1207aa148ee648bf34b2a17055822c4c6d9a
Gereon Steffens <github@steffens.org>
2024-12-17 11:22:51 +0100

    day 17

--- Sources/Day17/Day17.swift
+++ Sources/Day17/Day17.swift
@@ -5,18 +5,111 @@
 //
 
 import AoCTools
+import Foundation
+import Collections
 
-final class Day17: AOCDay {
-    let title = ""
-    
-    init(input: String) {
+private final class ChronospatialComputer {
+    let program: [Int]
+    let initialRegisters: [Int]
+
+    init(program: [Int], initialRegisters: [Int]) {
+        self.program = program
+        self.initialRegisters = initialRegisters
     }
 
-    func part1() -> Int {
-        0
+    func run() -> [Int] {
+        var output = [Int]()
+        var instructionPointer = 0
+        var registers = initialRegisters
+
+        func combo(_ operand: Int) -> Int {
+            switch operand {
+            case 0...3: operand
+            case 4...6: registers[operand - 4]
+            default: fatalError()
+            }
+        }
+
+        while instructionPointer < program.count {
+            let opcode = program[instructionPointer]
+            let operand = program[instructionPointer + 1]
+            var nextInstruction = instructionPointer + 2
+
+            switch opcode {
+            case 0: // adv
+                registers[0] /= pow(2, combo(operand))
+            case 1: // bxl
+                registers[1] ^= operand
+            case 2: // bst
+                registers[1] = combo(operand) % 8
+            case 3: // jnz
+                if registers[0] != 0 {
+                    nextInstruction = operand
+                }
+            case 4: // bxc
+                registers[1] ^= registers[2]
+            case 5: // out
+                output.append(combo(operand) % 8)
+            case 6: // bdv
+                registers[1] = registers[0] / pow(2, combo(operand))
+            case 7: // cdv
+                registers[2] = registers[0] / pow(2, combo(operand))
+            default:
+                fatalError()
+            }
+            instructionPointer = nextInstruction
+        }
+
+        return output
+    }
+
+
+    private func pow(_ x: Int, _ y: Int) -> Int {
+        Int(Foundation.pow(Double(x), Double(y)))
+    }
+}
+
+final class Day17: AOCDay {
+    let title = "Chronospatial Computer"
+
+    private let cpu: ChronospatialComputer
+
+    init(input: String) {
+        let lines = input.lines
+        let regA = lines[0].allInts()[0]
+        let regB = lines[1].allInts()[0]
+        let regC = lines[2].allInts()[0]
+        let program = lines[4].allInts()
+
+        cpu = ChronospatialComputer(program: program, initialRegisters: [regA, regB, regC])
+    }
+
+    func part1() -> String {
+        cpu.run()
+            .map { "\($0)" }
+            .joined(separator: ",")
     }
 
     func part2() -> Int {
-        0
+        let expected = cpu.program
+
+        var variants = Deque<(regA: Int, digits: Int)>()
+        variants.append((regA: 0, digits: 1))
+
+        while let (testA, digits) = variants.popFirst() {
+            for i in 0...7 {
+                let nextA = testA << 3 + i
+                let cpu = ChronospatialComputer(program: cpu.program, initialRegisters: [nextA, 0, 0])
+                let output = cpu.run()
+                if output == expected.suffix(digits) {
+                    if digits == expected.count {
+                        return nextA
+                    }
+                    variants.append((regA: nextA, digits: digits + 1))
+                }
+            }
+        }
+
+        return -1
     }
 }
--- Tests/Day17Tests.swift
+++ Tests/Day17Tests.swift
@@ -6,6 +6,19 @@ import Testing
 @testable import AdventOfCode
 
 fileprivate let testInput = """
+Register A: 729
+Register B: 0
+Register C: 0
+
+Program: 0,1,5,4,3,0
+"""
+
+fileprivate let testInput2 = """
+Register A: 2024
+Register B: 0
+Register C: 0
+
+Program: 0,3,5,4,3,0
 """
 
 @Suite("Day 17 Tests")
@@ -13,24 +26,24 @@ struct Day17Tests {
     @MainActor @Test("Day 17 Part 1")
     func testDay17_part1() {
         let day = Day17(input: testInput)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == "4,6,3,5,6,3,5,2,1,0")
     }
 
     @MainActor @Test("Day 17 Part 1 Solution")
     func testDay17_part1_solution() {
         let day = Day17(input: Day17.input)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == "5,1,4,0,5,1,0,2,6")
     }
 
     @MainActor @Test("Day 17 Part 2")
     func testDay17_part2() {
-        let day = Day17(input: testInput)
-        #expect(day.part2() == 0)
+        let day = Day17(input: testInput2)
+        #expect(day.part2() == 117440)
     }
 
     @MainActor @Test("Day 17 Part 2 Solution")
     func testDay17_part2_solution() {
         let day = Day17(input: Day17.input)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == 202322936867370)
     }
 }
