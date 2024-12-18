0b345cbe57572611d5d191da9863ecf7f6772e1d
Gereon Steffens <github@steffens.org>
2024-12-18 10:33:33 +0100

    day 18

--- Sources/Day18/Day18.swift
+++ Sources/Day18/Day18.swift
@@ -7,16 +7,63 @@
 import AoCTools
 
 final class Day18: AOCDay {
-    let title = ""
+    let title = "RAM Run"
+
+    let bytes: [Point]
+    var ram = [Point: Character]()
 
     init(input: String) {
+        bytes = input.lines.map { $0.allInts() }.map { Point($0[0], $0[1]) }
     }
 
     func part1() -> Int {
-        0
+        part1(fallen: 1024, gridSize: 70)
     }
 
-    func part2() -> Int {
-        0
+    func part1(fallen: Int, gridSize: Int) -> Int {
+        let ram = emptyRAM(size: gridSize)
+        return findPath(through: ram, fallenBytes: bytes.prefix(fallen), gridSize: gridSize)
+    }
+
+    func part2() -> String {
+        part2(fallen: 1024, gridSize: 70)
+    }
+
+    func part2(fallen: Int, gridSize: Int) -> String {
+        let ram = emptyRAM(size: gridSize)
+        for prefix in fallen + 1 ..< bytes.count {
+            let bytes = bytes.prefix(prefix)
+            if findPath(through: ram, fallenBytes: bytes, gridSize: gridSize) == 0 {
+                return bytes.last!.description
+            }
+        }
+        return ""
+    }
+
+    private func findPath(through ram: [Point: Character], fallenBytes: any Sequence<Point>, gridSize: Int) -> Int {
+        var ram = ram
+        fallenBytes.forEach { ram[$0] = "#" }
+
+        let ramNavigator = RAMNavigator(ram: ram)
+        let pathfinder = AStarPathfinder(map: ramNavigator)
+        let path = pathfinder.shortestPath(from: .zero, to: Point(gridSize, gridSize))
+        return path.count
+    }
+
+    private func emptyRAM(size: Int) -> [Point: Character] {
+        let points = (0...size).flatMap { x in
+            (0...size).map { y in
+                Point(x, y)
+            }
+        }
+        return Dictionary(uniqueKeysWithValues: points.map { ($0, ".") })
+    }
+}
+
+private struct RAMNavigator: Pathfinding {
+    let ram: [Point: Character]
+
+    func neighbors(of point: Point) -> [Point] {
+        point.neighbors().filter { ram[$0] == "." }
     }
 }
--- Tests/Day18Tests.swift
+++ Tests/Day18Tests.swift
@@ -6,6 +6,31 @@ import Testing
 @testable import AdventOfCode
 
 fileprivate let testInput = """
+5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0
 """
 
 @Suite("Day 18 Tests")
@@ -13,24 +38,24 @@ struct Day18Tests {
     @MainActor @Test("Day 18 Part 1")
     func testDay18_part1() {
         let day = Day18(input: testInput)
-        #expect(day.part1() == 0)
+        #expect(day.part1(fallen: 12, gridSize: 6) == 22)
     }
 
     @MainActor @Test("Day 18 Part 1 Solution")
     func testDay18_part1_solution() {
         let day = Day18(input: Day18.input)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 348)
     }
 
     @MainActor @Test("Day 18 Part 2")
     func testDay18_part2() {
         let day = Day18(input: testInput)
-        #expect(day.part2() == 0)
+        #expect(day.part2(fallen: 12, gridSize: 6) == "6,1")
     }
 
     @MainActor @Test("Day 18 Part 2 Solution")
     func testDay18_part2_solution() {
         let day = Day18(input: Day18.input)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == "54,44")
     }
 }
