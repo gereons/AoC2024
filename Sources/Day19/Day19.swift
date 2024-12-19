c5c1273494cde46847246e75d4425a3bfe09edfc
Gereon Steffens <github@steffens.org>
2024-12-19 11:26:11 +0100

    day 19

--- Sources/Day19/Day19.swift
+++ Sources/Day19/Day19.swift
@@ -7,16 +7,47 @@
 import AoCTools
 
 final class Day19: AOCDay {
-    let title = ""
+    let title = "Linen Layout"
+
+    let colors: [String]
+    let towels: [String]
 
     init(input: String) {
+        let lines = input.lines
+        colors = lines[0].split(separator: ", ").map { String($0) }
+        towels = Array(lines.dropFirst(2))
     }
 
     func part1() -> Int {
-        0
+        towels
+            .map { waysToMake(towel: $0) }
+            .count { $0 > 0 }
     }
 
     func part2() -> Int {
-        0
+        towels
+            .map { waysToMake(towel: $0) }
+            .reduce(0, +)
+    }
+
+    private var cache = [String: Int]()
+    private func waysToMake(towel: String) -> Int {
+        if towel.isEmpty {
+            return 1
+        }
+
+        var ways = 0
+        for color in colors {
+            if let seen = cache[towel] {
+                return seen
+            }
+
+            if towel.hasPrefix(color) {
+                ways += waysToMake(towel: String(towel.dropFirst(color.count)))
+            }
+        }
+
+        cache[towel] = ways
+        return ways
     }
 }
--- Tests/Day19Tests.swift
+++ Tests/Day19Tests.swift
@@ -6,6 +6,16 @@ import Testing
 @testable import AdventOfCode
 
 fileprivate let testInput = """
+r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb
 """
 
 @Suite("Day 19 Tests")
@@ -13,24 +23,24 @@ struct Day19Tests {
     @MainActor @Test("Day 19 Part 1")
     func testDay19_part1() {
         let day = Day19(input: testInput)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 6)
     }
 
     @MainActor @Test("Day 19 Part 1 Solution")
     func testDay19_part1_solution() {
         let day = Day19(input: Day19.input)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 236)
     }
 
     @MainActor @Test("Day 19 Part 2")
     func testDay19_part2() {
         let day = Day19(input: testInput)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == 16)
     }
 
     @MainActor @Test("Day 19 Part 2 Solution")
     func testDay19_part2_solution() {
         let day = Day19(input: Day19.input)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == 643685981770598)
     }
 }
