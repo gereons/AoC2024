b2976cf1d561f8874a9bb9239566948d69e7dcde
Gereon Steffens <github@steffens.org>
2024-12-21 20:33:55 +0100

    day 21

--- Tests/Day21Tests.swift
+++ Tests/Day21Tests.swift
@@ -6,6 +6,11 @@ import Testing
 @testable import AdventOfCode
 
 fileprivate let testInput = """
+029A
+980A
+179A
+456A
+379A
 """
 
 @Suite("Day 21 Tests")
@@ -13,24 +18,24 @@ struct Day21Tests {
     @MainActor @Test("Day 21 Part 1")
     func testDay21_part1() {
         let day = Day21(input: testInput)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 126384)
     }
 
     @MainActor @Test("Day 21 Part 1 Solution")
     func testDay21_part1_solution() {
         let day = Day21(input: Day21.input)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 123096)
     }
 
     @MainActor @Test("Day 21 Part 2")
     func testDay21_part2() {
         let day = Day21(input: testInput)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == 154115708116294)
     }
 
     @MainActor @Test("Day 21 Part 2 Solution")
     func testDay21_part2_solution() {
         let day = Day21(input: Day21.input)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == 154517692795352)
     }
 }
