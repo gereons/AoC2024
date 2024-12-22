6f3c93edc30f5caa33251fdfc07d2b4a212b42d0
Gereon Steffens <github@steffens.org>
2024-12-22 11:06:37 +0100

    day 22

--- Sources/Day22/Day22.swift
+++ Sources/Day22/Day22.swift
@@ -6,17 +6,95 @@
 
 import AoCTools
 
+private struct Run: Hashable {
+    let changes: [Int]
+    let price: Int
+}
+
+private typealias PriceChange = (price: Int, change: Int)
+
 final class Day22: AOCDay {
-    let title = ""
+    let title = "Monkey Market"
+
+    let secrets: [Int]
 
     init(input: String) {
+        secrets = input.lines.map { Int($0)! }
     }
 
     func part1() -> Int {
-        0
+        secrets
+            .map {
+                getNthSecret(initial: $0, n: 2000)
+            }
+            .reduce(0, +)
     }
 
     func part2() -> Int {
-        0
+        // for each monkey: list of price changes and their resulting price
+        var monkeyRuns = [Int: [Run]]()
+
+        for (monkeyId, secret) in secrets.enumerated() {
+            let changes = priceChanges(initial: secret, n: 2000)
+
+            for run in changes.runs(ofLength: 4) {
+                let r = Run(changes: [run[0].change, run[1].change, run[2].change, run[3].change], price: run[3].price)
+                monkeyRuns[monkeyId, default: []].append(r)
+            }
+        }
+
+        // sum up the total price for each run of changes, but only one per monkey
+        var sums = [[Int]: Int]()
+        var monkeyChangeSeen = Set<[Int]>()
+        for (monkeyId, runs) in monkeyRuns {
+            for run in runs {
+                let key = [monkeyId] + run.changes
+                if monkeyChangeSeen.contains(key) {
+                    continue
+                }
+                sums[run.changes, default: 0] += run.price
+                monkeyChangeSeen.insert(key)
+            }
+        }
+
+        return sums.max(of: \.value)!
+    }
+
+    private func priceChanges(initial secret: Int, n: Int) -> [PriceChange] {
+        var secret = secret
+        var prevDigit = secret % 10
+        var results = [(Int, Int)]()
+        for _ in 0 ..< n {
+            secret = mixPrune(secret, secret * 64)
+            secret = mixPrune(secret, secret / 32)
+            secret = mixPrune(secret, secret * 2048)
+            let digit = secret % 10
+            results.append((price: digit, change: digit - prevDigit))
+            prevDigit = digit
+        }
+
+        return results
+    }
+
+    private func getNthSecret(initial secret: Int, n: Int) -> Int {
+        var secret = secret
+        for _ in 0 ..< n {
+            secret = mixPrune(secret, secret * 64)
+            secret = mixPrune(secret, secret / 32)
+            secret = mixPrune(secret, secret * 2048)
+        }
+        return secret
+    }
+
+    private func mixPrune(_ secret: Int, _ value: Int) -> Int {
+        (secret ^ value) % 16777216
+    }
+}
+
+private extension Array {
+    func runs(ofLength n: Int) -> [[Element]] {
+        (0 ... (count - n)).map {
+            Array(self[$0 ..< $0 + n])
+        }
     }
 }
--- Tests/Day22Tests.swift
+++ Tests/Day22Tests.swift
@@ -6,6 +6,17 @@ import Testing
 @testable import AdventOfCode
 
 fileprivate let testInput = """
+1
+10
+100
+2024
+"""
+
+fileprivate let testInput2 = """
+1
+2
+3
+2024
 """
 
 @Suite("Day 22 Tests")
@@ -13,24 +24,24 @@ struct Day22Tests {
     @MainActor @Test("Day 22 Part 1")
     func testDay22_part1() {
         let day = Day22(input: testInput)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 37327623)
     }
 
     @MainActor @Test("Day 22 Part 1 Solution")
     func testDay22_part1_solution() {
         let day = Day22(input: Day22.input)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 20215960478)
     }
 
     @MainActor @Test("Day 22 Part 2")
     func testDay22_part2() {
-        let day = Day22(input: testInput)
-        #expect(day.part2() == 0)
+        let day = Day22(input: testInput2)
+        #expect(day.part2() == 23)
     }
 
     @MainActor @Test("Day 22 Part 2 Solution")
     func testDay22_part2_solution() {
         let day = Day22(input: Day22.input)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == 2221)
     }
 }
