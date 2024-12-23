e3b0e961a92d4524b4607a07beefb3c3bf813ad1
Gereon Steffens <github@steffens.org>
2024-12-23 09:08:20 +0100

    day 23 part 1

--- Sources/Day23/Day23.swift
+++ Sources/Day23/Day23.swift
@@ -7,13 +7,37 @@
 import AoCTools
 
 final class Day23: AOCDay {
-    let title = ""
+    let title = "LAN Party"
+
+    let connections: Set<Pair<String, String>>
 
     init(input: String) {
+        var conn = Set<Pair<String, String>>()
+        for line in input.lines {
+            let parts = line.split(separator: "-").map { String($0) }
+            conn.insert(Pair(parts[0], parts[1]))
+            conn.insert(Pair(parts[1], parts[0]))
+        }
+        connections = conn
     }
 
     func part1() -> Int {
-        0
+        var trios = Set<Set<String>>()
+        for pair in connections {
+            let firstConn = connections.filter { $0.first == pair.first && $0.second != pair.second }.map { $0.second }
+            let secondConn = connections.filter { $0.first == pair.second && $0.second != pair.first }.map { $0.second }
+            let thirds = Set(firstConn).intersection(secondConn)
+            for third in thirds {
+                let trio = Set([pair.first, pair.second, third])
+                trios.insert(trio)
+            }
+        }
+
+        let ts = trios.filter { set in
+            set.contains { $0.hasPrefix("t") }
+        }
+
+        return ts.count
     }
 
     func part2() -> Int {
--- Tests/Day23Tests.swift
+++ Tests/Day23Tests.swift
@@ -6,6 +6,38 @@ import Testing
 @testable import AdventOfCode
 
 fileprivate let testInput = """
+kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn
 """
 
 @Suite("Day 23 Tests")
@@ -13,13 +45,13 @@ struct Day23Tests {
     @MainActor @Test("Day 23 Part 1", .tags(.testInput))
     func testDay23_part1() {
         let day = Day23(input: testInput)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 7)
     }
 
     @MainActor @Test("Day 23 Part 1 Solution")
     func testDay23_part1_solution() {
         let day = Day23(input: Day23.input)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 1173)
     }
 
     @MainActor @Test("Day 23 Part 2", .tags(.testInput))
