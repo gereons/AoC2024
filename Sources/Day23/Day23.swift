54640868f07b95498c30b7f425e1a6c801d5e317
Gereon Steffens <github@steffens.org>
2024-12-23 17:18:50 +0100

    speedup

--- Sources/Day23/Day23.swift
+++ Sources/Day23/Day23.swift
@@ -10,10 +10,12 @@ import Collections
 final class Day23: AOCDay {
     let title = "LAN Party"
 
-    let connections: Set<Pair<String, String>>
+    typealias Pair = AoCTools.Pair<String, String>
+
+    let connections: Set<Pair>
 
     init(input: String) {
-        var conn = Set<Pair<String, String>>()
+        var conn = Set<Pair>()
         for line in input.lines {
             let parts = line.split(separator: "-").map { String($0) }
             conn.insert(Pair(parts[0], parts[1]))
@@ -24,7 +26,14 @@ final class Day23: AOCDay {
 
     func part1() -> Int {
         var trios = Set<Set<String>>()
+        var seen = Set<Pair>()
         for pair in connections {
+            // skip "aa-bb" if we've already seen "bb-aa"
+            if seen.contains(Pair(pair.second, pair.first)) {
+                continue
+            }
+            seen.insert(pair)
+
             let firstConn = connections.filter { $0.first == pair.first && $0.second != pair.second }.map { $0.second }
             let secondConn = connections.filter { $0.first == pair.second && $0.second != pair.first }.map { $0.second }
             let thirds = Set(firstConn).intersection(secondConn)
@@ -33,7 +42,6 @@ final class Day23: AOCDay {
                 trios.insert(trio)
             }
         }
-
         let ts = trios.filter { set in
             set.contains { $0.hasPrefix("t") }
         }
@@ -50,7 +58,7 @@ final class Day23: AOCDay {
             let clique = findAllConnectedTo(start)
             cliques.insert(clique)
         }
-        
+
         return cliques
             .sorted(by: { $0.count > $1.count })
             .first!
@@ -65,28 +73,27 @@ final class Day23: AOCDay {
         while let candidate = candidates.popFirst() {
             // neighbors of our current candidate
             let neighbors = connections.filter { $0.first == candidate }
-            // only look at ones that are connected to everyone in out clique so far
-            let nextCandidates = neighbors.filter { isConnected($0.second, clique) }
-            for next in nextCandidates {
-                if !clique.contains(next.second) {
-                    clique.insert(next.second)
-                    candidates.append(next.second)
-                }
-            }
-
+            // only look at ones that are connected to everyone in our clique so far
+            var nextCandidates = neighbors
+                .map { $0.second }
+                .filter { !clique.contains($0) }
+                .filter { isConnected($0, toEveryoneIn: clique) }
+                .makeSet()
+            clique.formUnion(nextCandidates)
             // remove all entries from clique that aren't connected to everyone else any longer
-            let copy = clique
-            for c in copy {
-                if !isConnected(c, copy) {
-                    clique.remove(c)
+            for entry in clique {
+                if !isConnected(entry, toEveryoneIn: clique) {
+                    clique.remove(entry)
+                    nextCandidates.remove(entry)
                 }
             }
+            candidates.append(contentsOf: nextCandidates)
         }
 
         return clique
     }
 
-    private func isConnected(_ node: String, _ clique: Set<String>) -> Bool {
+    private func isConnected(_ node: String, toEveryoneIn clique: Set<String>) -> Bool {
         clique.allSatisfy { node == $0 || connections.contains(Pair(node, $0)) }
     }
 }
